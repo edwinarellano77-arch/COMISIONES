04e280a991cebbcc285858d9f5f708bd171da57c
edwinArellano <edwinarellano77@gmail.com>
2026-04-18 13:03:44 -0400

    VALIDACIONES FINAL

--- ventas.js
+++ ventas.js
@@ -1,6 +1,62 @@
 const VENTAS_BASE = 5;
 
+function validarVentas(){
+    let valor = recuperarTexto("txtVentas");
+    let error = document.getElementById("errVentas");
 
+    if(valor === ""){
+        error.textContent = "El número de ventas no puede estar vacío.";
+        return false;
+    }
+
+    if(isNaN(valor)){
+        error.textContent = "Solo se permiten números.";
+        return false;
+    }
+
+    if(valor.length > 5){
+        error.textContent = "Máximo 5 dígitos permitidos.";
+        return false;
+    }
+
+    error.textContent = "";
+    return true;
+}
+
+function validarSueldoBase(){
+    let valor = recuperarTexto("txtSueldoBase");
+    let error = document.getElementById("errSueldoBase");
+
+    if(valor === ""){
+        error.textContent = "El salario base no puede estar vacío.";
+        return false;
+    }
+
+    if(isNaN(valor)){
+        error.textContent = "Solo se permiten números.";
+        return false;
+    }
+
+    error.textContent = "";
+    return true;
+}
+function validarPrecio(){
+    let valor = recuperarTexto("txtPrecio");
+    let error = document.getElementById("errPrecio");
+
+    if(valor === ""){
+        error.textContent = "El precio del producto no puede estar vacío.";
+        return false;
+    }
+
+    if(isNaN(valor)){
+        error.textContent = "Solo se permiten números.";
+        return false;
+    }
+
+    error.textContent = "";
+    return true;
+}
 //CALCULAR COMISION
 function calcularComision(numeroVentas,precioProducto){
     let comision = 0;
@@ -12,22 +68,22 @@ function calcularComision(numeroVentas,precioProducto){
 }
 
 function calcular(){
-    // RECUPERA TEXTO DE CAJA DE TEXTO Y SE GUARDA EN VARIABLE COMPONENTE.
-    //let componenteSueldoBase = document.getElementById("txtSueldoBase");ESTE PASO SE SUPRIME CON LA FUNCION RECUPERAR TEXTO.
-   
-    
- 
-    // RECUPERA VALOR CAJAS DE TEXTO Y SE GUARDA EN VARIABLE STR.
-    //let sueldoBaseStr = componenteSueldoBase.value;
-    //let sueldoBaseStr = recuperarTexto("txtSueldoBase");
-    
+
+//VALIDACIONES
+let hayErrores = false;
+
+    if(!validarSueldoBase()) hayErrores = true;
+    if(!validarVentas()) hayErrores = true;
+    if(!validarPrecio()) hayErrores = true;
+
+    if(hayErrores) return;
 
     //CONVIERTE TEXTO A NUMERO
     let sueldoBase = recuperarFloat("txtSueldoBase");
     let numeroVentas = recuperarFloat("txtVentas");
     let precioProducto = recuperarFloat("txtPrecio");
 
-    // INVOCA FUNCION CALCULAR COMISION//RECIVE 2 VARIABLES Y RETORNA COMISION
+    // INVOCA FUNCION CALCULAR COMISION
     let comision = calcularComision(numeroVentas,precioProducto);
 
     // CALCULO SUELDO TOTAL
@@ -42,6 +98,5 @@ function calcular(){
     spSueldoBase.textContent = sueldoBase;
     spComision.textContent = comision;
     spTotal.textContent = total;
-
 
 }
